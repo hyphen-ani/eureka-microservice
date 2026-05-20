38006519626742061d9afc510db575b6becc0ee9
Anirudh <shivammm.hyphen@gmail.com>
2026-05-20 10:42:24 +0530

    add-api-server

--- order-service/src/main/java/com/anirudh/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/anirudh/order_service/controller/OrderController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/order")
+@RequestMapping("/core")
 @RequiredArgsConstructor
 @Slf4j
 public class OrderController {
