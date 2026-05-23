945c20804fbd83e2efd18d6efc5566afdb83a166
Anirudh <shivammm.hyphen@gmail.com>
2026-05-23 11:37:39 +0530

    add:zipkin

--- api-gateway/src/main/java/com/anirudh/api_gateway/configs/AppConfig.java
+++ api-gateway/src/main/java/com/anirudh/api_gateway/configs/AppConfig.java
@@ -1,26 +1,13 @@
-package com.anirudh.inventory_service.config;
+package com.anirudh.api_gateway.configs;
 
-import com.netflix.spectator.api.Meter;
 import feign.Capability;
 import feign.micrometer.MicrometerCapability;
 import io.micrometer.core.instrument.MeterRegistry;
-import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestClient;
 
 @Configuration
 public class AppConfig {
-
-    @Bean
-    public ModelMapper modelMapper(){
-        return new ModelMapper();
-    }
-
-    @Bean
-    public RestClient restClient(){
-        return RestClient.builder().build();
-    }
 
     @Bean
     public Capability capability(final MeterRegistry registry){
